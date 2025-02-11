ded0e301c959ec1b117e90311093f9e97651760b
Willd231 <dellinger.w3@gmail.com>
2025-02-11 16:24:18 -0500

    g

--- src/App.js
+++ src/App.js
@@ -1,119 +1,108 @@
 import './App.css';
-import React from 'react';
-import { useState } from 'react';
+import Linkedin from "./1.png"
+import GitHub from "./2.png"
+import mail from "./3.png"
+import React, { useState } from 'react';
 
-
-
-const linkedin = () =>{
+const openLinkedIn = () => {
   window.open('https://www.linkedin.com/in/will-dellinger-a77b6129b/');
 };
 
-const github = () => {
+const openGitHub = () => {
   window.open('https://github.com/Willd231');
 };
 
-const email = () => {
+const openEmail = () => {
   window.open('mailto:dellinger.w3@gmail.com');
 };
-
-
 
 function Header() {
   return (
     <div className="Header">
+      <div className="H-item" id="h-LinkedIn">
       
-      <div class = "H-item" id = "h-Projects"><button onClick={linkedin}>Linkedin</button></div>
-      <div class = "H-item" id = "h-Experience"><button onClick={github}>Github</button></div>
-      <div class = "H-item" id = "h-aboutme"><button onClick={email}>Email</button></div>
-      
+        <button onClick={openLinkedIn}><img src={Linkedin} alt="Linkedin" />
+        </button>
+      </div>
+      <div className="H-item" id="h-GitHub">
+        <button onClick={openGitHub}><img src = {GitHub} alt = "GitHub" /> </button>
+      </div>
+      <div className="H-item" id="h-Email">
+        <button onClick={openEmail}><img src = {mail} alt ="mail" /> </button>
+      </div>
     </div>
   );
 }
 
-
-function Title(){
-  return(<div id = "Title">
-    <h1>Will Dellinger</h1>
-  </div>)
+function Title() {
+  return (
+    <div id="Title">
+      <h1>Will Dellinger</h1>
+    </div>
+  );
 }
 
-
-
-function Projects({expand, setExpand}){
+function Projects({ expand, setExpand }) {
   const toggle = (item) => {
-    setExpand(expand === item ? null :item)
+    setExpand(expand === item ? null : item);
   };
-  return(
-    
-    <div className = "Projects">
+
+  return (
+    <div className="Projects">
       <button onClick={() => toggle('project1')}>Projects</button>
-      {expand === 'project1'} 
-      <div class  = "flexitem" id = "p3">
-
-      </div>
-      <div class  = "flexitem" id = "p2">
-
-      </div>
-      <div class  = "flexitem" id = "p3">
-
-      </div>
-      </div>
-    
-  )
+      {expand === 'project1' && (
+        <>
+          <div className="flexitem" id="p1"></div>
+          <div className="flexitem" id="p2"></div>
+          <div className="flexitem" id="p3"></div>
+        </>
+      )}
+    </div>
+  );
 }
 
-
-
-function Experience(){
-  return(
-    <div className ="Experience">
-      <div class = "container">
-      <h1></h1>
-      <div class = "flexitem" id = "experience1">      
-        <h3></h3>
-        <p></p>
+function Experience() {
+  return (
+    <div className="Experience">
+      <div className="container">
+        <h1>Experience</h1>
+        <div className="flexitem" id="experience1">
+          <h3>Job Title</h3>
+          <p>Description...</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-function AboutMe(){
-
-  return(
-    <div className = "Aboutme">
-      <div className = "cont">
-      <h1></h1>
-      <div id = "abm">
-      <p> </p>
-      </div>
+function AboutMe() {
+  return (
+    <div className="AboutMe">
+      <div className="cont">
+        <h1>About Me</h1>
+        <div id="abm">
+          <p>Hi, I am goated</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-
-function Footer(){
-return(<div class = "flex-container" ID = "Footer">
-  
-  <div class = "F-item" id = "contact1"></div>
-  <div class = "F-item" id = "contact2"></div>
-  <div class = "F-item" id = "contact3"></div>
-
+function Footer() {
+  return (
+    <div className="flex-container" id="Footer">
+      <div className="F-item" id="contact1"></div>
+      <div className="F-item" id="contact2"></div>
+      <div className="F-item" id="contact3"></div>
     </div>
-)
+  );
 }
-
-
-
 
 function App() {
   const [expand, setExpand] = useState(null);
-  <Projects expand={expand} setExpand={setExpand} />
 
   return (
     <div className="App">
-      
       {/* Background Layers */}
       <div className="Background">
         <div className="Stars"></div>
@@ -123,10 +112,9 @@ function App() {
 
       {/* Foreground Content */}
       <div className="Foreground">
-       
         <Title />
         <Header />
-        <Projects />
+        <Projects expand={expand} setExpand={setExpand} />
         <Experience />
         <AboutMe />
         <Footer />
@@ -134,7 +122,5 @@ function App() {
     </div>
   );
 }
-
-
 
 export default App;
