053971dfe1f4dcca04f92f7bd4175d72a4cf6221
Willd231 <dellinger.w3@gmail.com>
2025-02-11 16:23:29 -0500

    major changes

--- src/App.js
+++ src/App.js
@@ -1,140 +1,72 @@
 import './App.css';
 import React from 'react';
-import { useState } from 'react';
-
-
-
-const linkedin = () =>{
-  window.open('https://www.linkedin.com/in/will-dellinger-a77b6129b/');
-};
-
-const github = () => {
-  window.open('https://github.com/Willd231');
-};
-
-const email = () => {
-  window.open('mailto:dellinger.w3@gmail.com');
-};
-
-
+import im1 from "./waving.png";
 
 function Header() {
   return (
-    <div className="Header">
+    <nav id="Header">
       
-      <div class = "H-item" id = "h-Projects"><button onClick={linkedin}>Linkedin</button></div>
-      <div class = "H-item" id = "h-Experience"><button onClick={github}>Github</button></div>
-      <div class = "H-item" id = "h-aboutme"><button onClick={email}>Email</button></div>
-      
-    </div>
+      <button id="Projects">Projects</button>
+      <button id="Experience">Experience</button>
+      <button id="AboutMe">About Me</button>
+      <button id="contact">Contact</button>
+    </nav>
   );
 }
 
 
-function Title(){
-  return(<div id = "Title">
-    <h1>Will Dellinger</h1>
-  </div>)
-}
-
-
-
-function Projects({expand, setExpand}){
-  const toggle = (item) => {
-    setExpand(expand === item ? null :item)
-  };
+function Projects(){
   return(
-    
-    <div className = "Projects">
-      <button onClick={() => toggle('project1')}>Projects</button>
-      {expand === 'project1'} 
-      <div class  = "flexitem" id = "p3">
-
-      </div>
-      <div class  = "flexitem" id = "p2">
-
-      </div>
-      <div class  = "flexitem" id = "p3">
-
-      </div>
-      </div>
-    
+    <nav className='projects'>
+      <button id = "p1">Meal Dealer</button>
+      <button id = "p2">Diagnose Me</button>
+      <button id = "p3">Study Flash</button>
+    </nav>
   )
 }
 
 
 
 function Experience(){
-  return(
-    <div className ="Experience">
-      <div class = "container">
-      <h1></h1>
-      <div class = "flexitem" id = "experience1">      
-        <h3></h3>
-        <p></p>
-        </div>
-      </div>
-    </div>
-  )
+  return( <h1>Experience</h1>
+  );
 }
 
 function AboutMe(){
-
   return(
-    <div className = "Aboutme">
-      <div className = "cont">
-      <h1></h1>
-      <div id = "abm">
-      <p> </p>
-      </div>
-      </div>
-    </div>
+    <h1>About me</h1>
   )
 }
 
 
-function Footer(){
-return(<div class = "flex-container" ID = "Footer">
-  
-  <div class = "F-item" id = "contact1"></div>
-  <div class = "F-item" id = "contact2"></div>
-  <div class = "F-item" id = "contact3"></div>
-
-    </div>
-)
+function ScrollHeader() {
+  return(
+  <div id="Scroll"></div>
+);
 }
 
-
-
-
 function App() {
-  const [expand, setExpand] = useState(null);
-  <Projects expand={expand} setExpand={setExpand} />
-
   return (
-    <div className="App">
+    <div className = "App">
+    <div className="Section1">
       
-      {/* Background Layers */}
+      <Header />
+      <ScrollHeader />
+      <img id = "photo" src = {im1}></img>
       <div className="Background">
         <div className="Stars"></div>
         <div className="Twinkling"></div>
         <div className="Clouds"></div>
       </div>
 
-      {/* Foreground Content */}
-      <div className="Foreground">
-       
-        <Title />
-        <Header />
-        <Projects />
-        <Experience />
-        <AboutMe />
-        <Footer />
-      </div>
+      <div className="Foreground"></div>
+    </div>
+    <div className='Section2'>
+      <h1 id = "title">Projects</h1>
+      <Projects />
+    </div>
     </div>
   );
 }
-
-
 
 export default App;
